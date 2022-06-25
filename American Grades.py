dceced3df631df5a290fe0689a597eb1d0de6076
jhuaehd <joshuaehd.silvio.37@gmail.com>
2022-06-25 10:44:02 +0800

    modify If condition

--- American Grades.py
+++ American Grades.py
@@ -5,15 +5,15 @@ if 0 <= grade <= 10:
     check = round(grade * 2) / 2
     if grade == check:
         # Grade A
-        if grade <= 10 and grade >= 8.5:
+        if 10 >= grade >= 8.5:
             print("Grade A")
-        if grade <= 8 and grade >= 7.5:
+        if 8 >= grade >= 7.5:
             print("Grade B")
-        if grade <= 7 and grade >= 6.5:
+        if 7 >= grade >= 6.5:
             print("Grade C")
-        if grade <= 6 and grade >= 5.5:
+        if 6 >= grade >= 5.5:
             print("Grade D")
-        if grade < 5.5 and grade >= 0:
+        if 5.5 > grade >= 0:
             print("Grade F")
 
     else:
