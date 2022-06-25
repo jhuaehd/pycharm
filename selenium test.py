bfb0c009205ce7b5d124154cb0da50a27070726b
jhuaehd <joshuaehd.silvio.37@gmail.com>
2022-06-25 15:57:01 +0800

    playground - created file selenium - added feature

--- selenium test.py
+++ selenium test.py
@@ -1,17 +1,22 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
 
-# s = Service('C:\Program Files (x86)\chromedriver.exe')
-# browser = webdriver.Chrome(service=s)
-# url = 'https://www.google.com'
-# browser.get(url)
+import time
 
 PATH = Service("C:\Program Files (x86)\chromedriver.exe")
 driver = webdriver.Chrome(service=PATH)
 driver.get("https://techwithtim.net")
 print("This page is : ", driver.title)
 
-driver.close()
+search = driver.find_elements_by_name()
+search.send_keys("test")
+search.send_keys(Keys.RETURN)
+
+time.sleep(5)
+
+driver.quit()
+
 
 
 # fname = input()
